4706aad05e52c805e6f118087efdb39f9d0f4f67
Soyboiii <ulriksin@gmail.com>
2021-08-19 16:58:05 +0200

    Added maks from compendium

--- src/timeThurUke33/Kortstokk.java
+++ src/timeThurUke33/Kortstokk.java
@@ -2,9 +2,9 @@ package timeThurUke33;
 
 public class Kortstokk {
     public static void main(String[] args) {
-        int[] values = {9, 13, 5, 10};
+        int[] values = {9, 13, 5, 10, 13};
         System.out.println(findMin(values));
-
+        System.out.println(maks(values));
     }
 
     static int findMin(int[] innArr) {
@@ -20,4 +20,20 @@ public class Kortstokk {
 
         return index;
     }
+
+    static int maks(int[] a)  // a er en heltallstabell
+    {
+        if (a.length < 1)
+            throw new java.util.NoSuchElementException("Tabellen a er tom!");
+
+        int m = 0;  // indeks til foreløpig største verdi (m for maks)
+
+        for (int i = 1; i < a.length; i++) // obs: starter med i = 1
+        {
+            if (a[i] >= a[m]) m = i;  // indeksen oppdateres // oppdateres også til siste nå siden >= istedenfor >
+        }
+
+        return m;  // returnerer indeksen/posisjonen til største verdi
+
+    } // maks
 }
