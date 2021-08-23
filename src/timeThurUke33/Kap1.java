6be277053d1705e299dd55dc2ae68ecde4d07f8a
Soyboiii <ulriksin@gmail.com>
2021-08-23 10:11:28 +0200

    Ny maks3 metode

--- src/timeThurUke33/Kap1.java
+++ src/timeThurUke33/Kap1.java
@@ -57,6 +57,30 @@ public class Kap1 {
 
     } // maks // 1 + 2 + 1 + n + n-1 + 2(n-1) + x + x + 1 = (4n + 2x + 2)
 
+    public static int maks3(int[] a)  // versjon 3 av maks-metoden
+    {
+        int sist = a.length - 1;       // siste posisjon i tabellen
+        int m = 0;                     // indeks til største verdi
+        int maksverdi = a[0];          // største verdi
+        int temp = a[sist];            // tar vare på siste verdi
+        a[sist] = 0x7fffffff;          // legger tallet 2147483647 sist
+
+        for (int i = 0; ; i++)         // i starter med 0
+            if (a[i] >= maksverdi)       // denne blir sann til slutt
+            {
+                if (i == sist)             // sjekker om vi er ferdige
+                {
+                    a[sist] = temp;          // legger siste verdi tilbake
+                    return temp >= maksverdi ? sist : m;   // er siste størst?
+                }
+                else
+                {
+                    maksverdi = a[i];        // maksverdi oppdateres
+                    m = i;                   // m oppdateres
+                }
+            }
+    } // maks
+
     public static int[] minMaks(int[] a) {
         return new int[] {findMin(a), maks(a)};
     }
